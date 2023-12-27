b05bc539aad184c830ff5caf712a5114ba343d94
Bianca Teixeira <biancabt1102@gmail.com>
2023-12-27 08:59:33 -0300

    Initial Commit

---
+++ src/main/java/br/com/nunessports/Models/Produtos.java
@@ -0,0 +1,24 @@
+package br.com.nunessports.Models;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "TB_Produtos")
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class Produtos {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_produto")
+    private Long idProduto;
+    @Column(name = "nm_produto")
+    private String nomeProduto;
+    @Column(name = "ds_produto")
+    private String descricaoProduto;
+    @Column(name = "vl_produto")
+    private double precoProduto;
+}
